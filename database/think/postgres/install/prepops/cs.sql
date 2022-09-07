670a1adbe3ba4bd926f9cf375d122bd21d62eb1b
Dhruv Agarwal <dhruv.agarwal@highwirepress.com>
2022-09-07 13:36:43 +0530

    Fix files

--- database/think/postgres/install/prepops/cs.sql
+++ database/think/postgres/install/prepops/cs.sql
@@ -9,18 +9,6 @@ VALUES (0, 'Black-Listed'),
 (2, 'Moderate'),
 (3, 'Weak');
 
-INSERT INTO thdb.tk_cs_m_custCategoryMapping(tenantId, custCatId, category, typeId)
-VALUES (1, 'Advertiser', 1),
-(2, 'Agent', 1),
-(3, 'Contributor', 1),
-(4, 'Editor', 1),
-(5, 'Individual', 1),
-(6, 'Institution', 2),
-(7, 'Members', 2),
-(8, 'Society', 2),
-(9, 'Student', 1),
-(10, 'Agency', 3);
-
 INSERT INTO thdb.tk_cs_m_addressTypes(addrTypeId, type)
 VALUES (1, 'Primary'),
 (2, 'Billing'),
