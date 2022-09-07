c0083fa1eddb38e7ebbbd8e8ae75c10375bf299b
Dhruv Agarwal <dhruv.agarwal@highwirepress.com>
2022-09-07 12:41:03 +0530

    adding install file

---
+++ database/think/postgres/install/install_think.sql
@@ -0,0 +1,11 @@
+\i tables/iam.sql
+\i tables/cs.sql
+\i tables/inventory.sql
+\i tables/setup.sql
+
+\i prepops/iam.sql
+\i prepops/cs.sql
+\i prepops/inventory.sql
+\i prepops/setup.sql
+
+COMMIT;
